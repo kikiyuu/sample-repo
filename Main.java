9888b08fe47a1e6f776a16c07c4e429a9bd2e348
ykigure <ykigure@nssx.co.jp>
2025-03-21 18:14:07 +0900

    Mainファイルを追加

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello World!!");
+
+        int[] score = { 60, 20, 50, 70, 80 };
+        System.out.println(score[0]);
+
+    }
+}
